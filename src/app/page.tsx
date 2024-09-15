a55ccb444f01c030a9280abe52e3035dca130edc
J-su35 <det_a248@hotmail.com>
2024-09-15 23:02:28 +0700

    studied back day5 completed, no revise, no test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,88 +3,95 @@
 import BudgetPanel from "./components/BudgetPanel";
 import BudgetRequestDataTable from "./components/BudgetRequestDataTable";
 import Header from "./components/Header";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { BudgetRequest } from "./models/budget-request";
+import { createBudgetItem, fetchBudgetItems } from "./services/budget-item";
 
 let nextId = 3;
 function Home() {
-  //   const [budgetRequests, setBudgetRequests] = useState<BudgetRequests[]>([
+  // const [budgetRequests, setBudgetRequests] = useState<BudgetRequest[]>([
   //   {
-  //     image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+  //     id: 1,
   //     title: "Monitor",
-  //     amount: "$2500.00",
+  //     amount: 100,
+  //     quantity: 1,
+  //     status: "PENDING",
   //   },
   //   {
-  //     image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
-  //     title: "Hard Disk/SSD",
-  //     amount: "$2000.75",
+  //     id: 2,
+  //     title: "Ram",
+  //     amount: 200,
+  //     quantity: 1,
+  //     status: "APPROVED",
   //   },
-  //   {
-  //     image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
-  //     title: "Monitor",
-  //     amount: "$2500.00",
-  //   },
-  //   {
-  //     image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
-  //     title: "Hard Disk/SSD",
-  //     amount: "$2000.75",
+  //   // {
+  //   //   id: 3,
+  //   //   title: "CPU",
+  //   //   amount: 300,
+  //   //   quantity: 1,
+  //   //   status: "APPROVED",
+  //   // },
+  // ]);
+  const [budgetRequests, setBudgetRequests] = useState<BudgetRequest[]>([]);
+
+  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault()
+  //   setBudgetRequests([
+  //     ...budgetRequests, 
+  //     {
+  //     id: nextId++,
+  //     title: "",
+  //     amount: 0,
+  //     quantity: 1,
+  //     status: "PENDING",
   //   },
   // ]);
+  // };
+  useEffect(() => {
+    fetchBudgetItems().then((items) => setBudgetRequests(items));
+  }, []);
 
-  const [budgetRequests, setBudgetRequests] = useState<BudgetRequest[]>([
-    {
-      id: 1,
-      title: "Monitor",
-      amount: 100,
-      quantity: 1,
-      status: "PENDING",
-    },
-    {
-      id: 2,
-      title: "Ram",
-      amount: 200,
-      quantity: 1,
-      status: "APPROVED",
-    },
-    {
-      id: 3,
-      title: "CPU",
-      amount: 300,
-      quantity: 1,
-      status: "APPROVED",
-    },
-  ]);
+  // const addRequest = (newRequest: BudgetRequest) => {
+  //   setBudgetRequests([...budgetRequests, newRequest]);
+  // };
+  //new addRequest
+  const addRequest = async (newRequest: BudgetRequest) => {
+    const insertedRequest = await createBudgetItem({
+      title: newRequest.title,
+      quantity: newRequest.quantity,
+      amount: newRequest.amount,
+    });
+    setBudgetRequests([...budgetRequests, insertedRequest]);
+  };
+
   const [newRequest, setNewRequest] = useState<BudgetRequest>({
     id: 0,
     title: "",
     amount: 0,
     quantity: 1,
-    status: "PENDING",
+    status: "APPROVED",
   });
 
   const updateField = (event: ChangeEvent<HTMLInputElement>) => {
-    const value =
-    event.target.value === "number"
+    const value = 
+    event.target.type === "number"
     ? Number(event.target.value)
     : event.target.value;
     setNewRequest({
         ...newRequest,
         [event.target.name]: value,
       });
-  }
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setBudgetRequests([
-      ...budgetRequests, 
-      {
+    event.preventDefault();
+    addRequest({
       id: nextId++,
-      title: "",
-      amount: 0,
+      title: newRequest.title,
+      amount: newRequest.amount,
       quantity: 1,
-      status: "PENDING",
-    },
-  ]);
+      status: "APPROVED",
+    });
   };
 
   return (
@@ -94,7 +101,6 @@ function Home() {
         <div className="mt-4">
           <BudgetPanel items={budgetRequests}/>
         </div>
-        {/* old code */}
         <form onSubmit={handleSubmit}>
           <div>
             Title:
@@ -115,13 +121,48 @@ function Home() {
           </div>
           <button>Add</button>
         </form>
-        {/* old code */}
+        {/* <FormAddRequest addRequest={addRequest} /> */}
         <div className="mt-4">
-          <BudgetRequestDataTable items={budgetRequests}/>
+          <BudgetRequestDataTable items={budgetRequests} />
         </div>
       </main>
     </div>
   );
 }
+
+// interface FormAddRequestProps {
+//   addRequest(request: BudgetRequest): void;
+// }
+
+// function FormAddRequest(props: FormAddRequestProps) {
+//   const [newRequest, setNewRequest] = useState<BudgetRequest>({
+//     id: 0,
+//     title: "",
+//     amount: 0,
+//     quantity: 1,
+//     status: "APPROVED",
+//   });
+
+//   const updateField = (event: ChangeEvent<HTMLInputElement>) => {
+//     const value = 
+//     event.target.type === "number"
+//     ? Number(event.target.value)
+//     : event.target.value;
+//     setNewRequest({
+//         ...newRequest,
+//         [event.target.name]: value,
+//       });
+//   };
+
+  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   props.addRequest({
+  //     id: nextId++,
+  //     title: newRequest.title,
+  //     amount: newRequest.amount,
+  //     quantity: 1,
+  //     status: "APPROVED",
+  //   });
+  // };
 
 export default Home;
